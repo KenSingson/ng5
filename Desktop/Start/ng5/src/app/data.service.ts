5d9d860cf3e2ebf383605cc2578c159dd305e8fb
Ken Singson <KenSingson@example.com>
2018-02-04 13:12:01 +0800

    fourth commit

--- Desktop/Start/ng5/src/app/data.service.ts
+++ Desktop/Start/ng5/src/app/data.service.ts
@@ -4,7 +4,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class DataService {
 
-  private goals = new BehaviorSubject<any>(['The initial goal', 'Another silly goal']);
+  private goals = new BehaviorSubject<any>(['Raze to the ground the Great Walls of China ', 'Reach the top of Mount Everest .. naked']);
   goal = this.goals.asObservable();
 
   constructor() { }
